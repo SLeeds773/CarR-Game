8eb851bc438a8c0f308226ad8c8d732257f1fe7b
SLeeds773 <125769641+SLeeds773@users.noreply.github.com>
2023-04-04 17:05:08 -0400

    Trucks and Spawn rates editable
    Trucks are in and work the same as cars atm
    
    also I tried to make it easier to change the spawn rates for obstacles (and birds apparently)

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] ToSpawn;
+    public int spawnNum;
     // Start is called before the first frame update
     void Start()
     {
-        int SpawnOrNo = Random.Range(0,5);
+        int SpawnOrNo = Random.Range(0,spawnNum);
 
         Instantiate(ToSpawn[SpawnOrNo], this.transform.position, Quaternion.identity);
     }
